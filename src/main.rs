911e65be5b3e179c3acd08f54fb20ab2f24771b6
David Sunshine <david@sunshines.org>
2022-01-29 18:02:16 -0500

    Improved noise generation
    Improved the noise generation algorithm, the ground seems to be set to
    half of the z-depth so anything below this does not render.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,13 @@ struct Map {
     height: u8,
     noise_map: Vec<Vec<f64>>,
     scale: f64,
+    octaves: u8,
+    persistance: f64,
+    lacunarity: f64,
+}
+
+fn inverselerp(x: f64, a: f64, b: f64) -> f64 {
+    (x - a) / (b - a)
 }
 
 impl Map {
@@ -15,11 +22,29 @@ impl Map {
             height,
             noise_map: vec![vec![0.0; width.into()]; height.into()],
             scale: 0.0001,
+            octaves: 0,
+            persistance: 0.0001,
+            lacunarity: 0.0001,
         }
     }
 
     fn scale(mut self, scale: f64) -> Self {
         self.scale = scale;
+        self
+    }
+
+    fn octaves(mut self, octaves: u8) -> Self {
+        self.octaves = octaves;
+        self
+    }
+
+    fn persistance(mut self, persistance: f64) -> Self {
+        self.persistance = persistance;
+        self
+    }
+
+    fn lacunarity(mut self, lacunarity: f64) -> Self {
+        self.lacunarity = lacunarity;
         self
     }
 
@@ -30,14 +55,45 @@ impl Map {
             return Err("Scale must be greater than zero!".to_string());
         }
 
+        let mut max_noise_height = f64::MIN;
+        let mut min_noise_height = f64::MAX;
+
         for y in 0..self.height {
             for x in 0..self.width {
-                let sx = (x as f64) / self.scale;
-                let sy = (y as f64) / self.scale;
-                let sz = noise.get([sx, sy]);
-                self.noise_map[x as usize][y as usize] = sz;
+                let mut amplitude = 1.0;
+                let mut frequency = 1.0;
+                let mut noise_height = 1.0;
+                for _i in 0..self.octaves {
+                    let sx = ((x as f64) / self.scale) * frequency;
+                    let sy = ((y as f64) / self.scale) * frequency;
+                    let sz = noise.get([sx, sy]);
+
+                    noise_height += sz * amplitude;
+
+                    amplitude *= self.persistance;
+                    frequency *= self.lacunarity;
+                }
+
+                if noise_height > max_noise_height {
+                    max_noise_height = noise_height;
+                } else if noise_height < min_noise_height {
+                    min_noise_height = noise_height;
+                }
+                self.noise_map[x as usize][y as usize] = noise_height;
             }
         }
+
+        // Normalize
+        for y in 0..self.height {
+            for x in 0..self.width {
+                self.noise_map[x as usize][y as usize] = inverselerp(
+                    min_noise_height,
+                    max_noise_height,
+                    self.noise_map[x as usize][y as usize],
+                );
+            }
+        }
+
         Ok(self)
     }
 
@@ -45,16 +101,22 @@ impl Map {
 }
 
 fn main() {
-    let mut vox = VoxFile::new(100, 100, 100);
-    let map = Map::new(100, 100).scale(24.3).build().expect("WTF");
+    let mut vox = VoxFile::new(100, 100, 40);
+    let map = Map::new(100, 100)
+        .scale(24.3)
+        .octaves(4)
+        .persistance(0.5)
+        .lacunarity(2.0)
+        .build()
+        .expect("WTF");
+    vox.set_palette_color(1, 129, 108, 91, 255);
     for x in 0..100 {
         for y in 0..100 {
             let z = (map.noise_map[x][y] + 20.0) as u8;
-            let voxel = Voxel::new(x as u8, y as u8, z, 10);
+            let voxel = Voxel::new(x as u8, y as u8, z, 1);
             vox.models[0].add_voxel(voxel);
         }
     }
-    vox.set_palette_color(255, 255, 0, 0, 255);
 
     vox.save("firma.vox");
 }
